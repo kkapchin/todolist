84eae8dd9ef6fe1f2ad777649fb9591302a8a151
Константин Капчин <kkapchin@Konstantins-MacBook-Air.local>
2023-03-14 21:57:39 -0400

    Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,28 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <Todolist />
+        </div>
+    );
+}
+
+function Todolist() {
+    return (
+        <div>
+            <h3>What to learn</h3>
             <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+                <input />
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true} /> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true} /> <span>JS</span></li>
+                <li><input type="checkbox" checked={false} /> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
             </div>
         </div>
     );
