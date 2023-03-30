f1c68798a489ab91acaf6693ecb2b8406495c94b
kkapchin <kkapchin@yahoo.com>
2023-03-29 20:43:19 -0400

    refactor, uuid, ToDoList input

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,35 +1,51 @@
-import {Filter} from "./App";
-
-export type TaskType = {
-    id: number;
-    title: string;
-    isDone: boolean;
-}
+import {ChangeEvent, KeyboardEvent, useState} from "react";
+import { TaskType } from "./Types/task-type";
+import {Filter} from "./const";
 
 type TodolistProps = {
-    title: string;
-    tasks: Array<TaskType>;
-    removeTask: (id: number) => void;
-    changeFilter: (filter: Filter) => void;
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (id: string) => void
+    changeFilter: (filter: Filter) => void
+    addTask: (title: string) => void
 }
 
-export default function Todolist ({title, tasks, removeTask, changeFilter}: TodolistProps) {
+export default function Todolist({title, tasks, removeTask, changeFilter, addTask}: TodolistProps) {
+
+    const [newTitle, setNewTitle] = useState('')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(e.currentTarget.value)
+    }
+
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key !== 'Enter') {
+            return;
+        }
+        addTask(newTitle);
+        setNewTitle('');
+    }
+
     return (
         <div>
             <h3>{title}</h3>
             <div>
-                <input/>
+                <input onChange={onChangeHandler}
+                       onKeyDown={onKeyDownHandler}
+                       value={newTitle}
+                />
                 <button>+</button>
             </div>
             <ul>
-                {tasks.map((task) => {
+                {tasks.map(task => {
                     return (
                         <li key={task.id}>
                             <input type="checkbox" checked={task.isDone}/>
                             <span>{task.title} </span>
                             <button onClick={() => {
                                 removeTask(task.id);
-                            }}>✖️</button>
+                            }}>✖️
+                            </button>
                         </li>
                     );
                 })}
